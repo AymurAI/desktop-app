ab5cb3f53964807665201005563cc314d5127caf
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-08 13:58:30 -0300

    poc(app): add basic Google OAuth implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
+import { useGoogleLogin } from '@react-oauth/google';
+
 function App() {
+  const googleLogin = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      console.log(tokenResponse);
+      // fetching userinfo can be done on the client or the server
+    },
+    // scope: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+
   return (
     <div>
       <h1>Electron App</h1>
       <p>This is the starter code for an Electron desktop app!</p>
+      <button onClick={() => googleLogin()}>Login with Google</button>
     </div>
   );
 }
