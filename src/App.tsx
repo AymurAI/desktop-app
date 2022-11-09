1f3a489dace2523e2426d306df6afd427e94fc13
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-09 19:14:07 -0300

    chore(app): update main app with an example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { useGoogleLogin } from '@react-oauth/google';
+import { useState } from 'react';
+
+import google from 'services/google';
 
 function App() {
+  const [title, setTitle] = useState<string | null>(null);
   const googleLogin = useGoogleLogin({
-    onSuccess: async (tokenResponse) => {
-      console.log(tokenResponse);
-      // fetching userinfo can be done on the client or the server
+    onSuccess: async (res) => {
+      const data = await google(res.access_token)
+        // ID of the dataset
+        .spreadsheet('1uAi-Yfq-rJl_cqQaVe9Fv1rLlBJcmEtDpUB0NTOrLAs')
+        .data();
+
+      console.log(data);
+      setTitle(data.properties.title);
     },
-    // scope: 'https://www.googleapis.com/auth/spreadsheets',
   });
 
   return (
@@ -14,6 +22,9 @@ function App() {
       <h1>Electron App</h1>
       <p>This is the starter code for an Electron desktop app!</p>
       <button onClick={() => googleLogin()}>Login with Google</button>
+      <p>
+        Recibido el spreadsheet: <code>{title ?? '-'}</code>
+      </p>
     </div>
   );
 }
