22ff41e98f084a6cd00966b4e1de8ce496dba2c4
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-16 19:51:29 -0300

    feat: add navigation logic to login page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,29 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { Background, Container } from 'layout/login';
 import { Text, Title } from 'components';
+import { useGoogleToken, useLogin } from 'hooks';
 
 export default function Login() {
+  const navigate = useNavigate();
+  const token = useGoogleToken();
+
+  const { login } = useLogin();
+
+  /**
+   * Ensures the token state has been successfully updated before navigating to the `/home`
+   */
+  useEffect(() => {
+    if (token && token !== '') navigate('/home');
+  }, [token, navigate]);
+
   return (
     <Background>
       <Container>
         <Text>Te damos la bienvenida a</Text>
         <Title>AymurAI</Title>
-        <button>Login</button>
+        <button onClick={() => login()}>Login</button>
       </Container>
     </Background>
   );
