fc71a0fa726dfcc8d2819c09786bf175d0c74812
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-17 17:29:09 -0300

    feat: include user service to global google service

--- src/services/google/index.ts
+++ src/services/google/index.ts
@@ -2,11 +2,13 @@ import { GoogleToken } from 'types/google';
 
 import { SpreadsheetId } from 'types/spreadsheet';
 import { getSpreadsheet } from './spreadsheet';
+import { getUser } from './user';
 
 export default function google(token: GoogleToken) {
   return {
     spreadsheet: (id: SpreadsheetId) => ({
       data: () => getSpreadsheet(token, id),
     }),
+    user: () => getUser(token),
   };
 }
