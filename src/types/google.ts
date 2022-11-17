c76b7c997839ecf3acfafdade1ff7f7193e66afd
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-17 17:25:23 -0300

    feat: add GoogleUser type
    This type difers from User in that the last one also includes the access_token

--- src/types/google.ts
+++ src/types/google.ts
@@ -1,4 +1,21 @@
+import { TokenResponse } from '@react-oauth/google';
+
 /**
  * Token retrieved from OAuth2
  */
-export type GoogleToken = string;
+export type GoogleToken = TokenResponse['access_token'];
+
+/**
+ * User info retrieved from Google User API
+ */
+export type GoogleUser = {
+  email: string;
+  email_verified: boolean;
+  family_name: string;
+  given_name: string;
+  hd: string;
+  locale: string;
+  name: string;
+  picture: string;
+  sub: string;
+};
