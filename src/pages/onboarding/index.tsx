8d31b21271ae4398222fc19e7af1326e1ab4e347
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-22 15:22:06 -0300

    feat: add basic structure to Onboarding

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,3 +1,4 @@
+import { Button, Stack, Text, Title } from 'components';
 import { Section, Footer } from 'layout/main';
 
 export default function Onboarding() {
@@ -6,10 +7,21 @@ export default function Onboarding() {
     <>
       {/* Onboarding description */}
       <Section css={{ gap: '$xl' }}>
+        <Stack spacing="m">
+          <Title weight="strong">¿Cómo funciona AymurAI?</Title>
+          <Text>
+            Esta herramienta te permitirá subir las resoluciones del juzgado
+            para que sean analizadas por una inteligencia artificial que
+            extraerá la información relevante para el set de datos abiertos con
+            perspectiva de género.
+          </Text>
+        </Stack>
       </Section>
 
       {/* Input file */}
       <Footer>
+        <Text size="sm">Formatos válidos: .doc y .docx</Text>
+        <Button onClick={handleSelectFile}>Cargar documentos</Button>
       </Footer>
     </>
   );
