52b18dd0e04b98a2c467f5194707636556ea67a8
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-22 12:34:11 -0300

    feat: add support for script load detection

--- src/context/Authentication.tsx
+++ src/context/Authentication.tsx
@@ -7,6 +7,7 @@ import { User } from 'types/user';
 type AuthContextType = {
   user: User | null;
   setUser: (user: User | null) => void;
+  hasScriptLoaded: boolean;
 };
 /**
  * Provides the token received through OAuth2 login to the whole app
@@ -14,7 +15,9 @@ type AuthContextType = {
 export const AuthenticationContext = createContext<AuthContextType>({
   user: null,
   setUser: () => {},
+  hasScriptLoaded: false,
 });
+AuthenticationContext.displayName = 'AuthenticationContext';
 
 interface Props {
   children?: ReactNode;
@@ -24,14 +27,26 @@ interface Props {
  */
 export default function AuthProvider({ children }: Props) {
   const [user, setUser] = useState<User | null>(null);
+  const [hasScriptLoaded, setHasScriptLoaded] = useState(false);
 
   const updateUser = (user: User | null) => {
     setUser(user);
   };
 
+  const updateScriptStatus = () => {
+    setHasScriptLoaded(true);
+  };
+
   return (
-    <AuthenticationContext.Provider value={{ user, setUser: updateUser }}>
-      <GoogleOAuthProvider clientId={CLIENT_ID}>{children}</GoogleOAuthProvider>
+    <AuthenticationContext.Provider
+      value={{ user, setUser: updateUser, hasScriptLoaded }}
+    >
+      <GoogleOAuthProvider
+        clientId={CLIENT_ID}
+        onScriptLoadSuccess={updateScriptStatus}
+      >
+        {children}
+      </GoogleOAuthProvider>
     </AuthenticationContext.Provider>
   );
 }
