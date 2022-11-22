7ea312b923c18f609c2c0ba68b42acb4afa28b4f
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-22 15:28:07 -0300

    feat: add Cards to Onboarding page

--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -1,6 +1,10 @@
 import { Button, Stack, Text, Title } from 'components';
 import { Section, Footer } from 'layout/main';
 
+import { CardContainer, Card } from './cards';
+import { Input } from './Input';
+import RightArrow from './RightArrow';
+
 export default function Onboarding() {
 
   return (
@@ -16,10 +20,35 @@ export default function Onboarding() {
             perspectiva de género.
           </Text>
         </Stack>
+        <CardContainer>
+          <Card step={1} text="Selecciona los archivos" />
+          <RightArrow />
+          <Card
+            step={2}
+            text="La inteligencia artificial procesará los archivos"
+          />
+          <RightArrow />
+          <Card
+            step={3}
+            text="Valida que la información identificada sea correcta"
+          />
+          <RightArrow />
+          <Card
+            step={4}
+            text="Proceso terminado. Los archivos ya son parte del set de datos."
+          />
+        </CardContainer>
       </Section>
 
       {/* Input file */}
       <Footer>
+        <Input
+          type="file"
+          accept=".doc,.docx"
+          ref={inputRef}
+          onChange={handleAddFile}
+          multiple
+        />
         <Text size="sm">Formatos válidos: .doc y .docx</Text>
         <Button onClick={handleSelectFile}>Cargar documentos</Button>
       </Footer>
