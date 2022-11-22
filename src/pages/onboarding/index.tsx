00689f3d5358f142cf8fbf84e381b5ddb369fce9
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-22 15:20:56 -0300

    feat: add base Onboarding page

---
+++ src/pages/onboarding/index.tsx
@@ -0,0 +1,16 @@
+import { Section, Footer } from 'layout/main';
+
+export default function Onboarding() {
+
+  return (
+    <>
+      {/* Onboarding description */}
+      <Section css={{ gap: '$xl' }}>
+      </Section>
+
+      {/* Input file */}
+      <Footer>
+      </Footer>
+    </>
+  );
+}
