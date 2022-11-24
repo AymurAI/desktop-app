54420c0b7e2d69d333fa3b3556f39fe674240005
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-23 21:02:49 -0300

    feat: add Stepper export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,7 @@ import Button from './button';
 import Label from './label';
 import ProfileInfo from './profile-info';
 import Stack from './stack';
+import Stepper from './stepper';
 import Subtitle from './subtitle';
 import Text from './text';
 import ThemeProvider from './theme-provider';
@@ -12,6 +13,7 @@ export {
   Label,
   ProfileInfo,
   Stack,
+  Stepper,
   Subtitle,
   Text,
   ThemeProvider,
