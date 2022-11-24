2b9e16bdfdfbfcd101dd23bbd9202a8808b032e5
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-23 21:10:39 -0300

    feat: add FileContext to Main layout

--- src/layout/main/index.tsx
+++ src/layout/main/index.tsx
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import { Title, ProfileInfo } from 'components';
 import withAuthProtection from 'features/withAuthProtection';
+import FileContext from 'context/File';
 import { Props } from './Main.types';
 import { Section, Footer, Header, Layout } from './Main.styles';
 
 export { Section, Footer };
 
 export default withAuthProtection(function Main({ children, ...props }: Props) {
+  const [files, setFiles] = useState<File[]>([]);
+  const [step, setStep] = useState<number>(0);
+
+  const handleSetFiles = (newFiles: File[]) => setFiles(newFiles);
+  const handleSetStep = (newStep: number) => setStep(newStep);
+
   return (
     <Layout {...props}>
       <Header>
@@ -18,8 +26,17 @@ export default withAuthProtection(function Main({ children, ...props }: Props) {
         <ProfileInfo></ProfileInfo>
       </Header>
 
-      {/* Content as Outlet, managed by React-Router */}
-      <Outlet></Outlet>
+      <FileContext.Provider
+        value={{
+          files,
+          setFiles: handleSetFiles,
+          step,
+          setStep: handleSetStep,
+        }}
+      >
+        {/* Content as Outlet, managed by React-Router */}
+        <Outlet></Outlet>
+      </FileContext.Provider>
     </Layout>
   );
 });
