4779a1f408a929ea67a992fefe57e9b75fde4997
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-25 21:37:34 -0300

    feat: move file utils functions into their own files

---
+++ src/utils/file/index.ts
@@ -0,0 +1,5 @@
+import getExtension from './getExtension';
+import insertIntoHTML from './insertIntoHTML';
+import isFileAllowed from './isFileAllowed';
+
+export { getExtension, insertIntoHTML, isFileAllowed };
