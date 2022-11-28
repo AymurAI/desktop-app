f61f248c1e44c1c757bab7fd4196af08d83530b6
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-28 12:32:20 -0300

    feat: add toggleSelected file util function

--- src/utils/file/index.ts
+++ src/utils/file/index.ts
@@ -1,5 +1,6 @@
 import getExtension from './getExtension';
 import insertIntoHTML from './insertIntoHTML';
 import isFileAllowed from './isFileAllowed';
+import toggleSelected from './toggleSelected';
 
-export { getExtension, insertIntoHTML, isFileAllowed };
+export { getExtension, insertIntoHTML, isFileAllowed, toggleSelected };
