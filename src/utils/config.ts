68d815eadbf398bb9c5ae4da852cff8da458011a
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-30 20:28:14 -0300

    feat: add new values to config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,3 +2,13 @@
  * Google App Client ID (extracted from Google Developer Console)
  */
 export const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? '';
+
+/**
+ * AI Predict port
+ */
+export const PREDICT_PORT = 8899;
+
+/**
+ * Only allow these extensions to be analyzed
+ */
+export const WHITELISTED_EXTENSIONS = ['doc', 'docx'];
