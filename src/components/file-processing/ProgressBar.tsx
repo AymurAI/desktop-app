da889923b7243c6933ec625fdebce33a954ad075
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-30 21:30:30 -0300

    feat: create ProgressBar component

--- src/components/file-processing/ProgressBar.tsx
+++ src/components/file-processing/ProgressBar.tsx
@@ -1,17 +1,37 @@
 import { Label, Stack } from 'components';
+import { FileStatus } from 'hooks/usePredict';
 import { Bar, BarContainer } from './FileProcessing.styles';
 
 interface Props {
   fileName: string;
   progress: number;
+  status: FileStatus;
 }
-export default function ProgressBar({ fileName, progress }: Props) {
+export default function ProgressBar({ fileName, progress, status }: Props) {
+  const getProgressText = () => {
+    const text = {
+      processing: `${progress}%`,
+      completed: `${progress}%`,
+      error: 'Error de carga de archivo. Volvelo a intentar',
+      stopped: 'Detuviste el procesamiento de este archivo',
+    };
+
+    return text[status];
+  };
+
   return (
     <Stack direction="column" align="stretch" spacing="s" css={{ flex: 1 }}>
       {/* Texts */}
       <Stack justify="space-between">
-        <Label>{fileName}</Label>
-        <Label>{`${progress}%`}</Label>
+        <Label status={status === 'error' ? 'error' : 'default'}>
+          {fileName}
+        </Label>
+        <Label
+          status={status === 'error' ? 'error' : 'default'}
+          css={{ fontStyle: 'italic' }}
+        >
+          {getProgressText()}
+        </Label>
       </Stack>
 
       <BarContainer>
