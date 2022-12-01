1c9b55c99e298561ed05abce62ba6f5ceae5e82c
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-11-30 21:05:10 -0300

    feat-wip: add addFiles to useFiles hook

--- src/hooks/useFiles/index.ts
+++ src/hooks/useFiles/index.ts
@@ -1,12 +1,16 @@
 import { useContext } from 'react';
 
 import Context from 'context/File';
+import addFilesToState from './addFiles';
 
 export default function useFiles() {
   const { files, setFiles } = useContext(Context);
 
+  const addFiles = (newFiles: File[]) =>
+    setFiles(addFilesToState(newFiles, files));
 
   return {
     files,
+    addFiles,
   };
 }
