6e73c10844e5095f36216947b5143602e78d18d3
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-02 13:53:33 -0300

    docs: document file typing

--- src/types/file.ts
+++ src/types/file.ts
@@ -1,10 +1,23 @@
-import { PredictSuccess } from './predict';
+import { PredictLabel } from './predict';
 
 /**
  * Type for
  */
 export type DocFile = {
+  /**
+   * .doc or .docx file contents
+   */
   data: File;
+  /**
+   * Used on the 'preview' page to detect which files have to be processed
+   */
   selected: boolean;
-  predictions?: PredictSuccess['labels'];
+  /**
+   * Predictions made by the AI
+   */
+  predictions?: PredictLabel[];
+  /**
+   * Is the file validated by the validation form?
+   */
+  validated?: boolean;
 };
