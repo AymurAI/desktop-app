e46b4908bae20b416d5cf05e07834cb3e884a705
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-02 13:10:03 -0300

    feat: create Radio component

---
+++ src/components/radio/radio/index.tsx
@@ -0,0 +1,52 @@
+import { Circle } from 'phosphor-react';
+import { ChangeEventHandler, ReactNode, useState } from 'react';
+
+import { CSS } from 'styles';
+import { colors } from 'styles/tokens';
+import { Wrapper, Input, Radio as StyledRadio } from './Radio.styles';
+
+export interface Props {
+  children?: ReactNode;
+  name?: string;
+  checked?: boolean;
+  disabled?: boolean;
+  onChange?: ChangeEventHandler<HTMLInputElement>;
+  css?: CSS;
+}
+export default function Radio({
+  name,
+  checked = false,
+  disabled = false,
+  onChange,
+  css,
+  children,
+}: Props) {
+  const [isChecked, setIsChecked] = useState(checked);
+
+  const hasText = !!children;
+
+  const handleToggle: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setIsChecked(e.target.checked);
+    onChange?.(e);
+  };
+
+  const iconColor = disabled
+    ? colors.textOnButtonDisabled
+    : colors.textOnButtonAlternative;
+
+  return (
+    <Wrapper hasText={hasText} isDisabled={disabled} css={css}>
+      <Input
+        type="radio"
+        name={name}
+        checked={isChecked}
+        disabled={disabled}
+        onChange={handleToggle}
+      />
+      <StyledRadio>
+        <Circle color={iconColor} weight="fill" />
+      </StyledRadio>
+      {children}
+    </Wrapper>
+  );
+}
