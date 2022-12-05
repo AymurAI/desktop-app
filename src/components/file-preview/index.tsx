a8e8037accd334e54b68c2d946a48c18f6ea1240
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-05 10:15:32 -0300

    fix: replace old hook with new dispatch on FilePreview

--- src/components/file-preview/index.tsx
+++ src/components/file-preview/index.tsx
@@ -1,14 +1,15 @@
 import { Checkbox, Text } from 'components';
 import { FileContainer, Wrapper } from './FilePreview.styles';
 import { DocFile } from 'types/file';
-import { useFileParser, useFiles } from 'hooks';
+import { useFileDispatch, useFileParser } from 'hooks';
+import { toggleSelected } from 'reducers/file/actions';
 
 interface Props {
   file: DocFile;
 }
 
 export default function FilePreview({ file }: Props) {
-  const { toggleSelected } = useFiles();
+  const dispatch = useFileDispatch();
   // Converts the doc/docx file into HTML that can be inserted into a `<div>`
   const html = useFileParser(file.data);
 
@@ -17,7 +18,7 @@ export default function FilePreview({ file }: Props) {
       <Checkbox
         css={{ position: 'absolute', top: '$s', right: '$s' }}
         checked={file.selected}
-        onChange={() => toggleSelected(file.data.name)}
+        onChange={() => dispatch(toggleSelected(file.data.name))}
       ></Checkbox>
       <FileContainer dangerouslySetInnerHTML={{ __html: html ?? '' }} />
       <Text size="sm">{file.data.name}</Text>
