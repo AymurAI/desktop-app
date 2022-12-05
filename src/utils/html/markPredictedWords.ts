efb9671ec328080e4f9af88c1be84a48d737ebe6
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-05 11:47:02 -0300

    feat: style mark tags

--- src/utils/html/markPredictedWords.ts
+++ src/utils/html/markPredictedWords.ts
@@ -1,8 +1,8 @@
 /**
- * Mark the predicted words with a <mark> tag to highlight them
+ * Mark the predicted words with a `<mark>` tag to highlight them
  * @param html HTML content to be analyzed
  * @param words Words to be matched inside the HTML
- * @returns The same HTML but with the given words enclosed in <mark> tags
+ * @returns The same HTML but with the given words enclosed in `<mark>` tags
  */
 export default function markPredictedWords(html: string, words: string[]) {
   let replaced = html;
