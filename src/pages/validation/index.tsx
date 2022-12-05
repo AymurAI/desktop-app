b956ba4988a14ca03336c12948cad1554defea6a
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-05 17:25:15 -0300

    feat: add state to Validation page

--- src/pages/validation/index.tsx
+++ src/pages/validation/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
+
 import {
   Button,
   Grid,
   SectionTitle,
 } from 'components';
+import { useFiles } from 'hooks';
 import { Footer, Section } from 'layout/main';
 import withFileProtection from 'features/withFileProtection';
 
 export default withFileProtection(function Validation() {
+  const files = useFiles();
+  const [selected, setSelected] = useState(0);
+
+  const stepperEnabled = files.length > 1;
+  const nextFile = () => setSelected(selected + 1);
+  const previousFile = () => setSelected(selected - 1);
+
+  const selectedFile = files[selected];
 
   return (
     <>
