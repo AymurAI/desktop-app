a3323c40c8ac30d82d57f37bfbf567ed443fdf3e
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-05 17:23:55 -0300

    feat: improve Validation page structure

--- src/pages/validation/index.tsx
+++ src/pages/validation/index.tsx
@@ -1,5 +1,7 @@
 import {
+  Button,
   Grid,
+  SectionTitle,
 } from 'components';
 import { Footer, Section } from 'layout/main';
 import withFileProtection from 'features/withFileProtection';
@@ -8,12 +10,20 @@ export default withFileProtection(function Validation() {
 
   return (
     <>
-      <Grid columns={2} spacing="none" justify="stretch" align="stretch">
-        <Section css={{ px: 100 }} spacing="xxl">
+      <Grid
+        columns={2}
+        spacing="none"
+        justify="stretch"
+        align="stretch"
+        css={{ overflow: 'hidden' }}
+      >
+        <Section css={{ px: 100, overflowY: 'scroll' }} spacing="xxl">
+          <SectionTitle>3. Validación de datos</SectionTitle>
         </Section>
       </Grid>
       <Footer
       >
+        <Button size="l">Validar documento</Button>
       </Footer>
     </>
   );
