465cfbb1eadf54c105fbd4e097e54e4dccc0b7c1
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-06 11:56:24 -0300

    feat: add file check display on Finish page

--- src/pages/finish/index.tsx
+++ src/pages/finish/index.tsx
@@ -1,14 +1,39 @@
 import {
+  Card,
+  FileCheck,
+  Grid,
   SectionTitle,
+  Subtitle,
+  Text,
 } from 'components';
+import { useFileDispatch, useFiles } from 'hooks';
 import { Footer, Section } from 'layout/main';
 
 export default function Finish() {
+  const files = useFiles();
 
   return (
     <>
       <Section>
         <SectionTitle>4. Finalización</SectionTitle>
+        <Text>
+          Los datos encontrados por AymurAI y posteriormente validados ya son
+          parte del set de datos abiertos con perspectiva de género.
+        </Text>
+
+        <Card>
+          <Subtitle>Archivos procesados</Subtitle>
+          <Grid
+            columns={5}
+            spacing="xl"
+            justify="center"
+            css={{ width: '100%' }}
+          >
+            {files.map(({ data }) => (
+              <FileCheck fileName={data.name}></FileCheck>
+            ))}
+          </Grid>
+        </Card>
       </Section>
       <Footer>
       </Footer>
