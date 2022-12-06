fa42d612eb7feb303c6f2fbb91de03516f32edca
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-06 18:00:21 -0300

    feat: export Finish page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import Login from './login';
 import Onboarding from './onboarding';
 import Preview from './preview';
 import Process from './process';
+import Finish from './finish';
 
-export { Login, Onboarding, Preview, Process };
+export { Login, Onboarding, Preview, Process, Finish };
