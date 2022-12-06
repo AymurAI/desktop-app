7ce10bca77c4d71021fc76da6307f4460a53685d
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-06 20:46:59 -0300

    feat: add useImperativeHandler and forwardRef

--- src/components/select/index.tsx
+++ src/components/select/index.tsx
@@ -1,103 +1,116 @@
-import { useState, MouseEvent, KeyboardEvent } from 'react';
+import {
+  useState,
+  MouseEvent,
+  KeyboardEvent,
+  forwardRef,
+  useImperativeHandle,
+  ChangeEvent,
+} from 'react';
 import { CaretDown } from 'phosphor-react';
 
-import { Label, Suggestion, Text } from 'components';
+import { Label, Suggestion } from 'components';
 import {
   Container,
   Input,
+  InputContainer,
   Option,
   OptionContainer,
   TextContainer,
 } from './Select.styles';
+import { findOption } from './utils';
 
-type SelectOption = { id: string; text: string };
+export type SelectOption = { id: string; text: string };
 interface Props {
-  onChange?: (value: SelectOption | undefined) => void;
   options: SelectOption[];
   label?: string;
   helper?: string;
   selected?: SelectOption['id'];
-  suggestion?: SelectOption['id'];
+  suggestion?: SelectOption['text'];
 }
-export default function Select({
-  label,
-  helper,
-  onChange,
-  selected,
-  options,
-  suggestion,
-}: Props) {
-  const [selectedId, setSelectedId] = useState<SelectOption['id']>(
-    selected ?? ''
-  );
+export default forwardRef<{ selected: SelectOption | undefined }, Props>(
+  function Select({ label, helper, options, suggestion, selected }, ref) {
+    const [value, setValue] = useState<SelectOption['text']>(
+      findOption(selected, options)?.text ?? ''
+    );
 
-  const isValueEmpty = selectedId === '' || selectedId === undefined;
+    // Only exposes `selected` object to the parent component
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          selected: findOption(value, options),
+        };
+      },
+      [value, options]
+    );
 
-  const handleSelect =
-    (id: SelectOption['id']) => (e: MouseEvent<HTMLLIElement>) => {
-      e.currentTarget.blur();
-      setSelectedId(id);
+    const isValueEmpty = !value || value === '';
 
-      onChange?.(options.find((option) => id === option.id));
+    const handleClickSelect =
+      (text: string) => (e: MouseEvent<HTMLLIElement>) => {
+        setValue(text);
+        e.currentTarget.blur();
+      };
+
+    const handleKeySelect =
+      (text: string) => (e: KeyboardEvent<HTMLLIElement>) => {
+        if (e.code === 'Escape') {
+          e.currentTarget.blur();
+        } else if (e.code === 'Enter' || e.code === 'Space') {
+          e.preventDefault(); // Prevent scroll
+
+          setValue(text);
+          e.currentTarget.blur();
+        }
+      };
+
+    const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+      setValue(e.currentTarget.value);
     };
 
-  const handleKeySelect =
-    (id: SelectOption['id']) => (e: KeyboardEvent<HTMLLIElement>) => {
-      if (e.code === 'Escape') {
-        e.currentTarget.blur();
-      } else if (e.code === 'Enter' || e.code === 'Space') {
-        e.preventDefault();
-
-        setSelectedId(id);
-        e.currentTarget.blur();
-
-        onChange?.(options.find((option) => id === option.id));
-      }
-    };
-
-  const handleClickSuggestion = () => setSelectedId(suggestion ?? '');
-
-  const selectedOption = options.find(({ id }) => selectedId === id);
-  const suggestedOption = options.find(({ id }) => suggestion === id)?.text;
-
-  return (
-    <Container>
-      {/* READONLY 'INPUT' */}
-      <TextContainer>
-        {label && (
-          <Label size="s" css={{ color: '$textDefault' }}>
-            {label}
-          </Label>
-        )}
-
-        <Input tabIndex={0}>
-          <Label css={{ flex: 1, color: '$textDefault' }}>
-            {selectedOption?.text ?? ''}
-          </Label>
-          {suggestion && isValueEmpty && (
-            <Suggestion onClick={handleClickSuggestion}>
-              {suggestedOption}
-            </Suggestion>
+    return (
+      <Container>
+        <TextContainer>
+          {/* LABEL */}
+          {label && (
+            <Label size="s" css={{ color: '$textDefault' }}>
+              {label}
+            </Label>
           )}
-          <CaretDown size={16} />
-        </Input>
 
-        {helper && <Label size="s">{helper}</Label>}
-      </TextContainer>
+          {/* INPUT */}
+          <InputContainer tabIndex={0}>
+            <Input value={value} onChange={handleChangeInput} />
+            {suggestion && isValueEmpty && (
+              <Suggestion
+                onClick={handleClickSelect(suggestion)}
+                onKeyDown={handleKeySelect(suggestion)}
+                tabIndex={0}
+              >
+                {suggestion}
+              </Suggestion>
+            )}
+            <CaretDown size={16} />
+          </InputContainer>
 
-      {/* OPTION LIST */}
-      <OptionContainer>
-        {options.map(({ id, text }) => (
-          <Option
-            onClick={handleSelect(id)}
-            onKeyDown={handleKeySelect(id)}
-            key={id}
-            tabIndex={0}
-          >
-            {text}
-          </Option>
-        ))}
-      </OptionContainer>
-    </Container>
-  );
-}
+          {/* HELPER */}
+          {helper && <Label size="s">{helper}</Label>}
+        </TextContainer>
+
+        {/* OPTION LIST */}
+        <OptionContainer>
+          {options.map(({ id, text }) => (
+            <Option
+              onClick={handleClickSelect(text)}
+              onKeyDown={handleKeySelect(text)}
+              key={id}
+              tabIndex={0}
+            >
+              {text}
+            </Option>
+          ))}
+        </OptionContainer>
+      </Container>
+    );
+  }
+);
