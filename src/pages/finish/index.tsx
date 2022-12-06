6539cb1f1ff6150f755dd2f8283fe5ee71ec470b
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-06 11:53:38 -0300

    feat: create Finish page

---
+++ src/pages/finish/index.tsx
@@ -0,0 +1,17 @@
+import {
+  SectionTitle,
+} from 'components';
+import { Footer, Section } from 'layout/main';
+
+export default function Finish() {
+
+  return (
+    <>
+      <Section>
+        <SectionTitle>4. Finalización</SectionTitle>
+      </Section>
+      <Footer>
+      </Footer>
+    </>
+  );
+}
