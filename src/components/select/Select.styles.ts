912f0465b36b4d8819267f18aedca45cc602b820
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-06 22:10:37 -0300

    fix: change target for focus efect on Select component

--- src/components/select/Select.styles.ts
+++ src/components/select/Select.styles.ts
@@ -100,9 +100,10 @@ export const Container = styled('div', {
       opacity: 1,
       visibility: 'visible',
     },
-
-    outline: '1px solid $borderPrimaryAlt',
-    boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.16)',
-    borderRadius: '$xs',
+    [`& ${InputContainer}`]: {
+      outline: '1px solid $borderPrimaryAlt',
+      boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.16)',
+      borderRadius: '$xs',
+    },
   },
 });
