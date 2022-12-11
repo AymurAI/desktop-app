ca71de24babcd4f6022320b8ddaa22038a32781e
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-11 00:49:04 -0300

    feat: add write, append and read functions with Google API

--- src/services/google/index.ts
+++ src/services/google/index.ts
@@ -1,13 +1,22 @@
 import { GoogleToken } from 'types/google';
 
-import { SpreadsheetId } from 'types/spreadsheet';
-import { getSpreadsheet } from './spreadsheet';
+import { SpreadsheetId, WriteValue } from 'types/spreadsheet';
+import {
+  append,
+  getRange,
+  getSpreadsheetMetadata,
+  writeRange,
+} from './spreadsheet';
 import { getUser } from './user';
 
 export default function google(token: GoogleToken) {
   return {
     spreadsheet: (id: SpreadsheetId) => ({
-      data: () => getSpreadsheet(token, id),
+      metadata: () => getSpreadsheetMetadata(token, id),
+      range: (start: string, end: string) => getRange(token, id, start, end),
+      append: (values: WriteValue[]) => append(token, id, values),
+      write: (values: WriteValue[][], start: string, end: string) =>
+        writeRange(token, id, values, start, end),
     }),
     user: () => getUser(token),
   };
