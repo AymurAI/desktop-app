ff1fa699b93fee8cfe6c8ca9f1e63b70460f1fa2
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-12 10:01:34 -0300

    feat: add electron-forge packaging configuration

---
+++ forge.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  packagerConfig: {
+    // This forces forge to only package already builded files
+    ignore: ['^\\/public$', '^\\/src$', '^\\/node_modules$', '^\\/[.].+'],
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+};
