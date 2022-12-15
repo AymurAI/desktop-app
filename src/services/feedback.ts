1fab7269a35ea23e0c12cf8c5ed715847bc61022
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-14 21:02:10 -0300

    feat: create exportFeedback service

--- src/services/feedback.ts
+++ src/services/feedback.ts
@@ -1,4 +1,6 @@
 import { PredictionFeedback } from 'types/aymurai';
+import { DocFile } from 'types/file';
+import logger from 'utils/logger';
 
 function newLabel(
   name: string,
@@ -39,4 +41,24 @@ function joinValidation({
   );
 
   return result;
+}
+
+/**
+ * Exports the file validation and predictions as a JSON file
+ * @param file File to be exported
+ */
+export default async function exportFeedback(file: DocFile) {
+  const { name } = file.data;
+  const { filesystem } = window;
+
+  const feedback = joinValidation(file);
+
+  // Check if the prealod script was loaded successfully
+  if (filesystem) {
+    await filesystem.feedback.export(name, feedback);
+  } else {
+    logger.error(
+      'There was an error trying to use the "feedback" API, check your preload script'
+    );
+  }
 }
