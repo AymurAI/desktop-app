196870900025e0fa0c309fbb813fce049ff005b3
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-14 21:00:22 -0300

    feat: add joinValidation util function

--- src/services/feedback.ts
+++ src/services/feedback.ts
@@ -16,3 +16,27 @@ function newLabel(
     },
   };
 }
+
+function joinValidation({
+  predictions,
+  validationObject,
+}: DocFile): PredictionFeedback[] {
+  // First, add the predictions to the result array
+  const result: PredictionFeedback[] = predictions!.map((pred) => ({
+    // We are sure predictions is !undefined becasue this step is post file processing
+    ...pred,
+    validationText: validationObject[pred.attrs.aymurai_label] ?? null,
+  }));
+
+  // Get the labels from `validationObject` that aren't present on `preedictions`
+  const filtered = Object.keys(validationObject).filter((key) => {
+    return !predictions!.find((pred) => pred.attrs.aymurai_label === key);
+  });
+
+  // Append the missing labels to the result array
+  filtered.forEach((label) =>
+    result.push(newLabel(label, validationObject[label] ?? null))
+  );
+
+  return result;
+}
