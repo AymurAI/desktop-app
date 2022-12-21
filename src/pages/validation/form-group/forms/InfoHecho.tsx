a8387746c60e042b92eb94da9177b7534df455e9
Lucia Wainfeld <lucia.wainfeld@aerolab.co>
2022-12-21 10:52:58 -0300

    feat: replaced inputs with selects on closed fields

--- src/pages/validation/form-group/forms/InfoHecho.tsx
+++ src/pages/validation/form-group/forms/InfoHecho.tsx
@@ -30,40 +30,44 @@ export default function DatosDenunciante({
   const fraseName = (i: number) => `${LabelType.FRASES_AGRESION}${i}`;
 
   return (
-    <ValidationForm title="Información del hecho" fileName={fileName}>
+    <ValidationForm title='Información del hecho' fileName={fileName}>
       {(register) => (
         <>
           <Input
             {...register(LabelType.MATERIA)}
             defaultValue={getValue(LabelType.MATERIA)}
             suggestion={getSuggestion(LabelType.MATERIA)}
-            label="Materia"
+            label='Materia'
           />
-          <Input
+          <Select
             {...register(LabelType.ART_INFRINGIDO)}
-            defaultValue={getValue(LabelType.ART_INFRINGIDO)}
+            selected={getValue(LabelType.ART_INFRINGIDO)}
             suggestion={getSuggestion(LabelType.ART_INFRINGIDO)}
-            label="Artículo infringido"
+            label='Artículo infringido'
+            options={json.ART_INFRINGIDO}
           />
-          <Input
+          <Select
             {...register(LabelType.CODIGO_O_LEY)}
-            defaultValue={getValue(LabelType.CODIGO_O_LEY)}
+            selected={getValue(LabelType.CODIGO_O_LEY)}
             suggestion={getSuggestion(LabelType.CODIGO_O_LEY)}
-            label="Código o ley"
+            label='Código o ley'
+            options={json.CODIGO_O_LEY}
           />
-          <Input
+          <Select
             {...register(LabelType.CONDUCTA)}
-            defaultValue={getValue(LabelType.CONDUCTA)}
+            selected={getValue(LabelType.CONDUCTA)}
             suggestion={getSuggestion(LabelType.CONDUCTA)}
-            label="Conducta"
+            label='Conducta'
+            options={json.CONDUCTA}
           />
-          <Input
+          <Select
             {...register(LabelType.CONDUCTA_DESCRIPCION)}
-            defaultValue={getValue(LabelType.CONDUCTA_DESCRIPCION)}
+            selected={getValue(LabelType.CONDUCTA_DESCRIPCION)}
             suggestion={getSuggestion(LabelType.CONDUCTA_DESCRIPCION)}
-            label="Descripción de la conducta"
+            label='Descripción de la conducta'
+            options={json.CONDUCTA_DESCRIPCION}
           />
-          <RadioGroup name="violenciaGenero" label="Violencia de género">
+          <RadioGroup name='violenciaGenero' label='Violencia de género'>
             <Radio
               checked={getSuggestion(LabelType.VIOLENCIA_DE_GENERO) === 'si'}
               {...register(LabelType.VIOLENCIA_DE_GENERO_SI)}
@@ -77,7 +81,7 @@ export default function DatosDenunciante({
               No
             </Radio>
           </RadioGroup>
-          <CheckboxGroup name="tipoViolencia" title="Tipo de violencia">
+          <CheckboxGroup name='tipoViolencia' title='Tipo de violencia'>
             <Checkbox
               checked={getChecked(LabelType.V_FISICA)}
               {...register(LabelType.V_FISICA)}
@@ -132,9 +136,9 @@ export default function DatosDenunciante({
             selected={getValue(LabelType.MODALIDAD_DE_LA_VIOLENCIA)}
             suggestion={getSuggestion(LabelType.MODALIDAD_DE_LA_VIOLENCIA)}
             options={json.MODALIDAD_DE_LA_VIOLENCIA}
-            label="Modalidad de la violencia"
+            label='Modalidad de la violencia'
           />
-          <Stack direction="column" spacing="m" align="stretch">
+          <Stack direction='column' spacing='m' align='stretch'>
             {frasesArray.map((i) => (
               <Input
                 key={i}
@@ -142,13 +146,13 @@ export default function DatosDenunciante({
                 {...register(fraseName(i))}
                 defaultValue={getValue(fraseName(i))}
                 suggestion={getSuggestion(fraseName(i))}
-                label="Frases de la agresión"
+                label='Frases de la agresión'
               />
             ))}
             <Button
-              size="s"
+              size='s'
               css={{ alignSelf: 'flex-start' }}
-              variant="secondary"
+              variant='secondary'
               onClick={newFraseAgresion}
             >
               <Plus />
@@ -160,19 +164,20 @@ export default function DatosDenunciante({
             selected={getValue(LabelType.FRECUENCIA_EPISODIOS)}
             suggestion={getSuggestion(LabelType.FRECUENCIA_EPISODIOS)}
             options={json.FRECUENCIA_EPISODIOS}
-            label="Frecuencia del episodio"
+            label='Frecuencia del episodio'
           />
           <Input
             {...register(LabelType.ZONA_DEL_HECHO)}
             defaultValue={getValue(LabelType.ZONA_DEL_HECHO)}
             suggestion={getSuggestion(LabelType.ZONA_DEL_HECHO)}
-            label="Zona del hecho"
+            label='Zona del hecho'
           />
-          <Input
+          <Select
             {...register(LabelType.LUGAR_DEL_HECHO)}
-            defaultValue={getValue(LabelType.LUGAR_DEL_HECHO)}
+            selected={getValue(LabelType.LUGAR_DEL_HECHO)}
             suggestion={getSuggestion(LabelType.LUGAR_DEL_HECHO)}
-            label="Lugar del hecho"
+            label='Lugar del hecho'
+            options={json.LUGAR_DEL_HECHO}
           />
         </>
       )}
