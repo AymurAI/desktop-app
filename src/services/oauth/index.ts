c30b1385d093440fc049e29b0a317f945a7a56f4
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-22 11:24:23 -0300

    feat: export new oauth service

---
+++ src/services/oauth/index.ts
@@ -0,0 +1,11 @@
+import getNewToken from './getNewToken';
+import onceTokenReceived from './onceTokenReceived';
+import openConsentScreen from './openConsentScreen';
+
+const oauth = {
+  getNewToken,
+  onceTokenReceived,
+  openConsentScreen,
+};
+
+export default oauth;
