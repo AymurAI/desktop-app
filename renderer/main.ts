b25763064151408544684432cf56433aff35d965
Luciano Lapenna <luciano.lapenna@aerolab.co>
2022-12-22 11:37:04 -0300

    feat(renderer): send the auth_code to the app after signin

--- renderer/main.ts
+++ renderer/main.ts
@@ -1,8 +1,9 @@
-import { App, app, BrowserWindow } from 'electron';
+import { App, app, BrowserWindow, dialog } from 'electron';
 
 import createWindow from './createWindow';
 import { debug, installExtensions } from './extensions';
 import { setDefaultProtocol } from './protocol';
+import { sendCodeToApp } from './utils/oauth';
 
 /**
  * Configures the app with handlers and other features
@@ -19,6 +20,17 @@ export function configureApp(app: App) {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+
+  // Handle the protocol. In this case, we choose to show an Error Box.
+  app.on('open-url', (_, url) => {
+    const parsed = new URL(url);
+
+    if (parsed.protocol === 'aymurai.app:') {
+      sendCodeToApp(parsed);
+    } else {
+      dialog.showErrorBox('Welcome Back', `You arrived from: ${url}`);
+    }
   });
 
   // On macOS it's common for applications and their menu bar to stay active until the user quits
