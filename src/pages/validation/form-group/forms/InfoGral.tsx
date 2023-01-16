e59e8718ab365446ad0956828ad1678dc75e1f5f
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-16 11:44:39 -0300

    feat-wip: use new suggestion function on InfoGral form

--- src/pages/validation/form-group/forms/InfoGral.tsx
+++ src/pages/validation/form-group/forms/InfoGral.tsx
@@ -1,46 +1,48 @@
 import { Input, ValidationForm } from 'components';
 import { LabelType } from 'types/aymurai';
 import { FormProps } from '../FormGroup.types';
+import preds from 'utils/predictions';
 
 export default function InfoGral({
   getSuggestion,
   onSubmit,
   register,
+  predictions,
 }: FormProps) {
   return (
     <ValidationForm title="Información general" onSubmit={onSubmit}>
       <Input
         ref={register(LabelType.N)}
-        suggestion={getSuggestion(LabelType.N)}
+        {...preds(predictions).getTextSuggestion(LabelType.N)}
         label="Número"
         type="number"
       />
       <Input
         ref={register(LabelType.NRO_REGISTRO)}
-        suggestion={getSuggestion(LabelType.NRO_REGISTRO)}
+        {...preds(predictions).getTextSuggestion(LabelType.NRO_REGISTRO)}
         label="Número de registro"
         type="number"
       />
       <Input
         ref={register(LabelType.TOMO)}
-        suggestion={getSuggestion(LabelType.TOMO)}
+        {...preds(predictions).getTextSuggestion(LabelType.TOMO)}
         label="Tomo"
         type="number"
       />
       <Input
         ref={register(LabelType.FECHA_RESOLUCION)}
-        suggestion={getSuggestion(LabelType.FECHA_RESOLUCION)}
+        {...preds(predictions).getTextSuggestion(LabelType.FECHA_RESOLUCION)}
         label="Fecha de resolución"
       />
       <Input
         ref={register(LabelType.N_EXPTE_EJE)}
-        suggestion={getSuggestion(LabelType.N_EXPTE_EJE)}
+        {...preds(predictions).getTextSuggestion(LabelType.N_EXPTE_EJE)}
         label="Nro de expediente"
         type="number"
       />
       <Input
         ref={register(LabelType.FIRMA)}
-        suggestion={getSuggestion(LabelType.FIRMA)}
+        {...preds(predictions).getTextSuggestion(LabelType.FIRMA)}
         label="Firma"
       />
     </ValidationForm>
