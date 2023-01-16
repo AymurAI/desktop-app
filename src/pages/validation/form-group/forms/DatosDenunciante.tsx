84333cabe7f5832a9facb3b6c589e5a194d719e6
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-16 11:45:05 -0300

    feat-wip: use new suggestion function on DatoDenunciante form

--- src/pages/validation/form-group/forms/DatosDenunciante.tsx
+++ src/pages/validation/form-group/forms/DatosDenunciante.tsx
@@ -1,5 +1,6 @@
 import { Input, Select, ValidationForm } from 'components';
 import { LabelType } from 'types/aymurai';
+import preds from 'utils/predictions';
 import { FormProps } from '../FormGroup.types';
 import json from './options.json';
 
@@ -7,32 +8,39 @@ export default function DatosDenunciante({
   onSubmit,
   register,
   getSuggestion,
+  predictions,
 }: FormProps) {
   return (
     <ValidationForm title="Datos del denunciante" onSubmit={onSubmit}>
       <Select
         ref={register(LabelType.GENERO_DENUNCIANTE)}
-        suggestion={getSuggestion(LabelType.GENERO_DENUNCIANTE)}
+        {...preds(predictions).getSelectSuggestion(
+          LabelType.GENERO_DENUNCIANTE
+        )}
         label="Género"
         options={json.GENERO}
       />
 
       <Select
         ref={register(LabelType.NACIONALIDAD_DENUNCIANTE)}
-        suggestion={getSuggestion(LabelType.NACIONALIDAD_DENUNCIANTE)}
+        {...preds(predictions).getSelectSuggestion(
+          LabelType.NACIONALIDAD_DENUNCIANTE
+        )}
         label="Nacionalidad"
         options={json.NACIONALIDAD}
       />
       <Input
         ref={register(LabelType.EDAD_DENUNCIANTE)}
-        suggestion={getSuggestion(LabelType.EDAD_DENUNCIANTE)}
+        {...preds(predictions).getTextSuggestion(LabelType.EDAD_DENUNCIANTE)}
         label="Edad"
         helper="Al momento del hecho"
         type="number"
       />
       <Select
         ref={register(LabelType.NIVEL_INSTRUCCION_DENUNCIANTE)}
-        suggestion={getSuggestion(LabelType.NIVEL_INSTRUCCION_DENUNCIANTE)}
+        {...preds(predictions).getSelectSuggestion(
+          LabelType.NIVEL_INSTRUCCION_DENUNCIANTE
+        )}
         label="Nivel de instrucción"
         options={json.NIVEL_INSTRUCCION}
       />
