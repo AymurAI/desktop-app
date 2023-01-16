0ef8de5bcbc22527b979ce4a51c7b6a6bbb1b1b5
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-16 20:54:09 -0300

    feat: add nArray util function

---
+++ src/utils/nArray.ts
@@ -0,0 +1,11 @@
+/**
+ * Creates an ordered array with a length of `n`
+ * @param n Amount of values
+ * @param base Value to store on each element of the array
+ * @returns A `number[]` of `n` length
+ */
+export default function nArray<T extends any = undefined>(n = 1, value: T) {
+  const array = Array.from(Array(n).keys()).map(() => value);
+
+  return array;
+}
