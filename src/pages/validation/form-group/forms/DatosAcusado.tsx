0faa75fa80a4ba6844aa10ac9afa2ef88414114a
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-16 11:45:29 -0300

    feat-wip: use new suggestion function on DatoAcusado form

--- src/pages/validation/form-group/forms/DatosAcusado.tsx
+++ src/pages/validation/form-group/forms/DatosAcusado.tsx
@@ -1,5 +1,6 @@
 import { Input, Select, ValidationForm } from 'components';
 import { LabelType } from 'types/aymurai';
+import preds from 'utils/predictions';
 import { FormProps } from '../FormGroup.types';
 import json from './options.json';
 
@@ -7,37 +8,44 @@ export default function DatosAcusado({
   onSubmit,
   register,
   getSuggestion,
+  predictions,
 }: FormProps) {
   return (
     <ValidationForm title="Datos del acusado/a" onSubmit={onSubmit}>
       <Select
         ref={register(LabelType.GENERO_ACUSADO)}
-        suggestion={getSuggestion(LabelType.GENERO_ACUSADO)}
+        {...preds(predictions).getSelectSuggestion(LabelType.GENERO_ACUSADO)}
         label="Género"
         options={json.GENERO}
       />
       <Select
         ref={register(LabelType.PERSONA_ACUSADA_NO_DETERMINADA)}
-        suggestion={getSuggestion(LabelType.PERSONA_ACUSADA_NO_DETERMINADA)}
+        {...preds(predictions).getSelectSuggestion(
+          LabelType.PERSONA_ACUSADA_NO_DETERMINADA
+        )}
         label="Persona acusada no determinada"
         options={json.PERSONA_ACUSADA_NO_DETERMINADA}
       />
       <Select
         ref={register(LabelType.NACIONALIDAD_ACUSADO)}
-        suggestion={getSuggestion(LabelType.NACIONALIDAD_ACUSADO)}
+        {...preds(predictions).getSelectSuggestion(
+          LabelType.NACIONALIDAD_ACUSADO
+        )}
         label="Nacionalidad"
         options={json.NACIONALIDAD}
       />
       <Input
         ref={register(LabelType.EDAD_ACUSADO)}
-        suggestion={getSuggestion(LabelType.EDAD_ACUSADO)}
+        {...preds(predictions).getTextSuggestion(LabelType.EDAD_ACUSADO)}
         type="number"
         label="Edad"
         helper="Al momento del hecho"
       />
       <Select
         ref={register(LabelType.NIVEL_INSTRUCCION_ACUSADO)}
-        suggestion={getSuggestion(LabelType.NIVEL_INSTRUCCION_ACUSADO)}
+        {...preds(predictions).getSelectSuggestion(
+          LabelType.NIVEL_INSTRUCCION_ACUSADO
+        )}
         label="Nivel de instrucción"
         options={json.NIVEL_INSTRUCCION}
       />
