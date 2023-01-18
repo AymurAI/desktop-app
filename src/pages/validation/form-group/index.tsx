59fd74a6873575bb793ad5c69b1e52bb95e78fc5
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-18 16:46:47 -0300

    feat-wip: set a suggester instance and send it as a prop

--- src/pages/validation/form-group/index.tsx
+++ src/pages/validation/form-group/index.tsx
@@ -9,15 +9,11 @@ import {
   Decision,
 } from './forms';
 import Container from './FormGroup.styles';
-import { GetCheckedFunction, GetSuggestionFunction } from './FormGroup.types';
 import { DecisionTabs } from 'components';
 import { useFileDispatch, useForm } from 'hooks';
 import { appendValidation } from 'reducers/file/actions';
-import { countDecisiones } from 'utils/predictions';
+import { countDecisiones, Suggester } from 'utils/predictions';
 
-type Predictions<T extends string | string[]> = {
-  [key: string]: T;
-};
 interface Props {
   file: DocFile;
 }
@@ -28,15 +24,6 @@ export default function FormGroup({ file }: Props) {
   );
   const dispatch = useFileDispatch();
 
-  // Format the orediction into a { [string]: string } format
-  const predictions: Predictions<string> = (file.predictions ?? []).reduce(
-    (prev, { attrs, text }) => ({
-      ...prev,
-      [attrs.aymurai_label]: text,
-    }),
-    {}
-  );
-
   const createDecision = () => {
     addDecision();
   };
@@ -46,17 +33,10 @@ export default function FormGroup({ file }: Props) {
     dispatch(appendValidation(file.data.name, data));
   });
 
-  const getSuggestion: GetSuggestionFunction = (label) =>
-    predictions[label] ?? '';
-  const getChecked: GetCheckedFunction = (label) =>
-    getSuggestion(label).toLowerCase() === 'si';
-
   const props = {
-    getSuggestion,
-    getChecked,
     register,
     onSubmit: handleSubmit,
-    predictions: file.predictions,
+    suggester: new Suggester(file.predictions),
   };
 
   return (
