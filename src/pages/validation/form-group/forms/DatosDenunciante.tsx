04f388c1041485fc5419ad68209a3f8d2765d8e0
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-19 12:47:26 -0300

    feat: add suggester to DatosDenunciante form

--- src/pages/validation/form-group/forms/DatosDenunciante.tsx
+++ src/pages/validation/form-group/forms/DatosDenunciante.tsx
@@ -1,42 +1,38 @@
 import { Input, Select, ValidationForm } from 'components';
 import { LabelType } from 'types/aymurai';
-import { suggest } from 'utils/predictions';
 import { FormProps } from '../FormGroup.types';
 import json from './options.json';
 
 export default function DatosDenunciante({
   onSubmit,
   register,
-  getSuggestion,
-  predictions,
+  suggester,
 }: FormProps) {
   return (
     <ValidationForm title="Datos del denunciante" onSubmit={onSubmit}>
       <Select
         ref={register(LabelType.GENERO_DENUNCIANTE)}
-        {...suggest(predictions).select(LabelType.GENERO_DENUNCIANTE)}
+        {...suggester.select(LabelType.GENERO_DENUNCIANTE)}
         label="Género"
         options={json.GENERO}
       />
 
       <Select
         ref={register(LabelType.NACIONALIDAD_DENUNCIANTE)}
-        {...suggest(predictions).select(LabelType.NACIONALIDAD_DENUNCIANTE)}
+        {...suggester.select(LabelType.NACIONALIDAD_DENUNCIANTE)}
         label="Nacionalidad"
         options={json.NACIONALIDAD}
       />
       <Input
         ref={register(LabelType.EDAD_DENUNCIANTE)}
-        {...suggest(predictions).text(LabelType.EDAD_DENUNCIANTE)}
+        {...suggester.text(LabelType.EDAD_DENUNCIANTE)}
         label="Edad"
         helper="Al momento del hecho"
         type="number"
       />
       <Select
         ref={register(LabelType.NIVEL_INSTRUCCION_DENUNCIANTE)}
-        {...suggest(predictions).select(
-          LabelType.NIVEL_INSTRUCCION_DENUNCIANTE
-        )}
+        {...suggester.select(LabelType.NIVEL_INSTRUCCION_DENUNCIANTE)}
         label="Nivel de instrucción"
         options={json.NIVEL_INSTRUCCION}
       />
