e066deaae219da3dd55af5ff07d03c35dc2467e6
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-19 13:00:45 -0300

    feat: add suggester to Decision form

--- src/pages/validation/form-group/forms/Decision.tsx
+++ src/pages/validation/form-group/forms/Decision.tsx
@@ -9,14 +9,12 @@ import {
 import { LabelDecisiones } from 'types/aymurai';
 import { FormProps } from '../FormGroup.types';
 import json from './options.json';
-import { suggest } from 'utils/predictions';
 
 export default function Decision({
   register,
-  getSuggestion,
   decision,
   onSubmit,
-  predictions,
+  suggester,
 }: FormProps) {
   const prop = (label: LabelDecisiones) => register(label, decision);
 
@@ -24,64 +22,64 @@ export default function Decision({
     <ValidationForm title="Decisión/es" onSubmit={onSubmit}>
       <Select
         ref={prop(LabelDecisiones.TIPO_DE_RESOLUCION)}
-        {...suggest(predictions).select(LabelDecisiones.TIPO_DE_RESOLUCION)}
+        {...suggester.select(LabelDecisiones.TIPO_DE_RESOLUCION)}
         label="Tipo de la resolución"
         options={json.TIPO_DE_RESOLUCION}
       />
       <Select
         ref={prop(LabelDecisiones.OBJETO_DE_LA_RESOLUCION)}
-        {...suggest(predictions).select(
-          LabelDecisiones.OBJETO_DE_LA_RESOLUCION
-        )}
+        {...suggester.select(LabelDecisiones.OBJETO_DE_LA_RESOLUCION)}
         options={json.OBJETO_DE_LA_RESOLUCION}
         label="Objeto de resolución"
       />
       <Select
         ref={prop(LabelDecisiones.DETALLE)}
-        {...suggest(predictions).select(LabelDecisiones.DETALLE)}
+        {...suggester.select(LabelDecisiones.DETALLE)}
         label="Detalle"
         options={json.DETALLE}
       />
       <Select
         ref={prop(LabelDecisiones.DECISION)}
-        {...suggest(predictions).select(LabelDecisiones.DECISION)}
+        {...suggester.select(LabelDecisiones.DECISION)}
         options={json.DECISION}
         label="Decisión"
       />
       <RadioGroup name="tipoAudiencia">
         <Radio
-          checked={
-            getSuggestion(LabelDecisiones.TIPO_DE_AUDIENCIA_ORAL) === 'oral'
-          }
+          // TODO gestionar este campo, debería depender de 'hora_inicio" y 'hora_cierre'
+          // checked={
+          //   getSuggestion(LabelDecisiones.TIPO_DE_AUDIENCIA_ORAL) === 'oral'
+          // }
           ref={prop(LabelDecisiones.TIPO_DE_AUDIENCIA_ORAL)}
         >
           Oral
         </Radio>
         <Radio
-          checked={
-            getSuggestion(LabelDecisiones.TIPO_DE_AUDIENCIA_ESCRITA) ===
-            'escrita'
-          }
+          // checked={
+          //   getSuggestion(LabelDecisiones.TIPO_DE_AUDIENCIA_ESCRITA) ===
+          //   'escrita'
+          // }
           ref={prop(LabelDecisiones.TIPO_DE_AUDIENCIA_ESCRITA)}
         >
           Escrita
         </Radio>
       </RadioGroup>
-      <Stack spacing="l" css={{ '&>*': { flex: 1 } }}>
+      {/* TODO ajustar posicionamiento de estos botones que se ponen en vertical cuando hay una sugerencia */}
+      <Stack spacing="l" css={{ '&>*': { flex: 1 } }} wrap="nowrap">
         <Input
           ref={prop(LabelDecisiones.HORA_DE_INICIO)}
-          {...suggest(predictions).text(LabelDecisiones.HORA_DE_INICIO)}
+          {...suggester.text(LabelDecisiones.HORA_DE_INICIO)}
           label="Hora de inicio"
         />
         <Input
           ref={prop(LabelDecisiones.HORA_DE_CIERRE)}
-          {...suggest(predictions).text(LabelDecisiones.HORA_DE_CIERRE)}
+          {...suggester.text(LabelDecisiones.HORA_DE_CIERRE)}
           label="Hora de cierre"
         />
       </Stack>
       <Input
         ref={prop(LabelDecisiones.DURACION)}
-        {...suggest(predictions).text(LabelDecisiones.DURACION)}
+        {...suggester.text(LabelDecisiones.DURACION)}
         label="Duración"
       />
     </ValidationForm>
