c73ba0493b59b3b65bf6e77ca2274eb65888c5a8
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-24 16:45:12 -0300

    feat: add discrimated union to User type

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,9 +1,13 @@
 import { GoogleUser, GoogleToken } from './google';
+import { XOR } from './utils';
 
+export type OfflinUser = { online: false; token: string };
+export type OnlineUser = {
+  online: true;
+  token: GoogleToken;
+  refreshToken: GoogleToken;
+} & GoogleUser;
 /**
  * Type referring to the info retrieved by Google API and the acess token from OAuth2
  */
-export type User = GoogleUser & {
-  token: GoogleToken;
-  refreshToken: GoogleToken;
-};
+export type User = XOR<OfflinUser, OnlineUser>;
