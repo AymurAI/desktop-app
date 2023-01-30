f05f857e5f119e8af6e0461cae853a2a1c81aaac
Luciano Lapenna <luciano.lapenna@aerolab.co>
2023-01-30 19:21:30 -0300

    feat: add data submission and login on Callout component

--- src/pages/finish/Callout.tsx
+++ src/pages/finish/Callout.tsx
@@ -1,9 +1,11 @@
-import { Info } from 'phosphor-react';
+import { CheckCircle, Info } from 'phosphor-react';
 
 import { colors } from 'styles/tokens';
 import { styled } from 'styles';
 import { Button, Stack, Subtitle } from 'components';
-import { useUser } from 'hooks';
+import { useFiles, useLogin, useUser } from 'hooks';
+import { submitValidations } from 'utils/file';
+import { useEffect, useRef, useState } from 'react';
 
 const Component = styled('div', {
   display: 'flex',
@@ -20,13 +22,48 @@ const Component = styled('div', {
 });
 
 export default function Callout() {
+  const [submitted, setSubmitted] = useState<boolean>();
   const user = useUser();
+  const files = useFiles();
+  const initialFlow = useRef<boolean>(!!user?.online);
+  const { login } = useLogin();
+
+  useEffect(() => {
+    // We changed modes, now is the moment to POST the data
+    if (user && user.online !== initialFlow.current) {
+      submitValidations({
+        isOnline: user.online,
+        token: user.token,
+        validations: files.map((v) => v.validationObject),
+      }).then(() => setSubmitted(true));
+    }
+  }, [user, files]);
+
+  const isOnline = initialFlow.current;
 
   const props = {
-    subtitle: user?.online
+    subtitle: isOnline
       ? 'Tus datos se guardaron en tu cuenta de Google ¿Quieres guardarlo en tu local también?'
       : 'Tus datos se guardaron en un carpeta en tu local ¿Quieres guardarlo en tu cuenta de Google también?',
-    buttonText: user?.online ? 'Guardar en local' : 'Guardar en Google',
+    buttonText: isOnline ? 'Guardar en local' : 'Guardar en Google',
+    // If we switched modes, this means we have POSTed the data to the cloud
+    buttonDisabled: submitted,
+  };
+
+  const handleCreateBackup = async () => {
+    if (isOnline) {
+      // We are working in online mode, must write to the filesystem
+      await submitValidations({
+        isOnline: false,
+        token: '',
+        validations: files.map((v) => v.validationObject),
+      });
+
+      setSubmitted(true);
+    } else {
+      // We are working in local, must upload the data to the cloud
+      login.online();
+    }
   };
 
   return (
@@ -38,8 +75,16 @@ export default function Callout() {
         </Subtitle>
       </Stack>
       {/* TODO agregar el onClick a este boton */}
-      <Button size="s" variant="secondary">
+      <Button
+        size="s"
+        variant="secondary"
+        onClick={handleCreateBackup}
+        disabled={props.buttonDisabled}
+      >
         {props.buttonText}
+        {submitted && (
+          <CheckCircle size={18} color={colors.textOnButtonDisabled} />
+        )}
       </Button>
     </Component>
   );
