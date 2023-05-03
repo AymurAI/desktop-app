2fe734094fda0156f91bf401d39d5d506e29dd04
Luciano Lapenna <lucianoglapenna@gmail.com>
2023-05-03 13:27:44 -0300

    feat: create taskbar API

--- renderer/utils/index.ts
+++ renderer/utils/index.ts
@@ -2,5 +2,6 @@ import resolveHTMLPath from './resolveHTMLPath';
 import feedback from './feedback';
 import excel from './excel';
 import crypto from './crypto';
+import taskbar from './taskbar';
 
-export { resolveHTMLPath, feedback, crypto, excel };
+export { resolveHTMLPath, feedback, crypto, excel, taskbar };
