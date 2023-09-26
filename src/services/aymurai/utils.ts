d635efccd6ef646f57ac79f46d6d1defe23346cc
Sergio Sabino <sergio.sabino@aerolab.co>
2023-09-26 14:28:41 -0400

    feat: anonimizer UI

--- src/services/aymurai/utils.ts
+++ src/services/aymurai/utils.ts
@@ -3,6 +3,5 @@ import axios from "axios";
 import { PREDICT_PORT } from "utils/config";
 
 export const fetcher = axios.create({
-  // baseURL: `http://localhost:${PREDICT_PORT}`,
-  baseURL: `http://192.168.100.99:${PREDICT_PORT}`,
+  baseURL: `http://localhost:${PREDICT_PORT}`,
 });
