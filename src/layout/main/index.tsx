f69485f1ebd6c5447f78a5ab31e4ab8ba279b2ab
Sergio Sabino <sergio.sabino@aerolab.co>
2023-10-20 15:20:22 -0400

    feat: anonymizer title

--- src/layout/main/index.tsx
+++ src/layout/main/index.tsx
@@ -1,20 +1,25 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
-import { Title, ProfileInfo, Stepper } from 'components';
-import withAuthProtection from 'features/withAuthProtection';
-import { Props } from './Main.types';
-import { Section, Footer, Header, Layout } from './Main.styles';
-import FileProvider from 'context/File';
+import { Title, ProfileInfo, Stepper } from "components";
+import withAuthProtection from "features/withAuthProtection";
+import { Props } from "./Main.types";
+import { Section, Footer, Header, Layout } from "./Main.styles";
+import FileProvider from "context/File";
+import { useUser } from "hooks";
+import { FunctionType } from "types/user";
 
 export { Section, Footer };
 
 export default withAuthProtection(function Main({ children, ...props }: Props) {
+  const user = useUser();
+
   return (
     <Layout {...props}>
       <Header>
         {/* Title & Profile picture & Logout */}
         <Title weight="strong" css={{ fontSize: 24 }}>
-          AymurAI
+          AymurAI{" "}
+          {user?.function === FunctionType.ANONYMIZER ? "Anonimizador" : ""}
         </Title>
         <Stepper></Stepper>
         <ProfileInfo></ProfileInfo>
