9b47e7cd68273c06559ed2a3688b3f9ade6435dc
Luciano Lapenna <lucianoglapenna@gmail.com>
2024-03-15 13:46:54 -0300

    feat(file container): make auto scroll on word change and add debounce

--- src/components/search-bar/useScroll.ts
+++ src/components/search-bar/useScroll.ts
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+
+const SCROLL_DEBOUNCE = 300;
 
 /**
  * Hook to scroll to the searched word
  * @param count Count of words found in the html
  */
-export default function useScroll(count: number) {
+export default function useScroll(count: number, word: string) {
+  const timer = useRef<number | null>(null);
+
   const scrollToComponent = (n: number) => {
     const index = n - 1;
     const components = document.querySelectorAll('mark.searched-word');
@@ -17,7 +21,22 @@ export default function useScroll(count: number) {
     });
   };
 
+  const clear = () => {
+    if (timer.current) {
+      clearTimeout(timer.current);
+      timer.current = null;
+    }
+  };
+
   useEffect(() => {
-    scrollToComponent(count);
+    if (word.length >= 2) {
+      setTimeout(() => scrollToComponent(1), SCROLL_DEBOUNCE);
+    }
+    return () => clear();
+  }, [word]);
+
+  useEffect(() => {
+    setTimeout(() => scrollToComponent(count), SCROLL_DEBOUNCE);
+    return () => clear();
   }, [count]);
 }
