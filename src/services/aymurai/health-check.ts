43b04a3802bb3f358f7a532547bd37a9fb76d620
unknown <lucia.wainfeld@aerolab.co>
2024-10-30 15:15:43 -0300

    feat(services): added api call for healthcheck endpoint

---
+++ src/services/aymurai/health-check.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { AYMURAI_API_URL } from "utils/config";
+
+/**
+ * Sends a file to the backend to be analyzed and retrieve a list of paragraphs
+ * @param serverUrl url on which the backend is running (may be localhost of an internal server)
+ * @returns a http status
+ */
+export async function getHealthCheck(serverUrl: string): Promise<boolean> {
+  try {
+    const response = await axios
+      .create({
+        baseURL: !!serverUrl ? serverUrl : AYMURAI_API_URL,
+      })
+      .get("/server/healthcheck");
+    return response.status === 200;
+  } catch (e) {
+    const { message } = e as Error;
+    console.error("Could not connect to server: ", message);
+    return false;
+  }
+}
