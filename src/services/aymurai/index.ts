546569e7d739b3627d278027dd743e59a5ed2041
Nicolás Poore <nicolaspoore@gmail.com>
2025-04-09 15:01:36 -0300

    Revert "Merge branch '181-anonymizer-integrar-api-de-validacion' into 'develop'"
    This reverts commit 4de91ce206f66bac47866d0ad4de2f9585c38af8, reversing
    changes made to df3dabff551a25f88d6f65ce7ac9135bf77d1516.

--- src/services/aymurai/index.ts
+++ src/services/aymurai/index.ts
@@ -1,6 +1,5 @@
 import predict from "./predict";
-export { predict };
 export { getParagraphs } from "./document-extract";
 export { anonymize } from "./anonymize";
 export { getHealthCheck } from "./health-check";
-export { validateAnonymizer } from "./validate/validate-anonimyze";
+export { predict };
