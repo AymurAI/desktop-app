d1edd543a3e031fd708bd646838e2165b9d7c9f7
AymurAI Frontend <front-end@datagenero.org>
2025-05-29 23:48:20 -0300

    chore(design): make input component fully controlled

--- src/components/design/input/index.tsx
+++ src/components/design/input/index.tsx
@@ -1,8 +1,8 @@
 import {
-	type ChangeEventHandler,
-	type KeyboardEventHandler,
-	type MouseEventHandler,
-	useRef,
+  type ChangeEventHandler,
+  type KeyboardEventHandler,
+  type MouseEventHandler,
+  useRef,
 } from "react";
 
 import { Label, Suggestion, Text } from "components";
@@ -11,117 +11,117 @@ import type { CSS } from "styles";
 import * as S from "./Input.styles";
 
 interface InputProps
-	extends Omit<
-		React.InputHTMLAttributes<HTMLInputElement>,
-		"prefix" | "type" | "onChange"
-	> {
-	label?: string;
-	helper?: string;
-	suggestion?: {
-		value: string;
-		label: string;
-	};
-	type?: "text" | "number";
-	css?: CSS;
+  extends Omit<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    "prefix" | "type" | "onChange" | "value"
+  > {
+  value: string;
+  label?: string;
+  helper?: string;
+  suggestion?: {
+    value: string;
+    label: string;
+  };
+  type?: "text" | "number";
+  css?: CSS;
 
-	prefix?: React.ReactNode;
-	suffix?: React.ReactNode;
+  prefix?: React.ReactNode;
+  suffix?: React.ReactNode;
 
-	onChange?: (value: string) => void;
+  onChange: (value: string) => void;
 }
 
 export default function Input({
-	label,
-	helper,
-	suggestion,
-	css,
-	prefix,
-	suffix,
-	value,
-	type = "text",
-	onChange,
-	...props
+  label,
+  helper,
+  suggestion,
+  css,
+  prefix,
+  suffix,
+  value,
+  type = "text",
+  onChange,
+  ...props
 }: InputProps) {
-	const inputRef = useRef<HTMLInputElement>(null);
-	const isValueEmpty = !value || value === "";
+  const inputRef = useRef<HTMLInputElement>(null);
+  const isValueEmpty = !value || value === "";
 
-	const isValid = (value: string): boolean =>
-		Boolean(type === "number" ? !value || value.match(/^\d*$/) : true);
+  const isValid = (value: string): boolean =>
+    Boolean(type === "number" ? !value || value.match(/^\d*$/) : true);
 
-	const applySuggestion = (suggestion: InputProps["suggestion"]) => {
-		if (!suggestion || !inputRef.current) return;
+  const applySuggestion = (suggestion: InputProps["suggestion"]) => {
+    if (!suggestion || !inputRef.current) return;
 
-		if (!isValid(suggestion.value)) {
-			console.error(
-				"Invalid suggestion. Tried to apply a 'string' to a 'number' input",
-			);
-			return;
-		}
+    if (!isValid(suggestion.value)) {
+      console.error(
+        "Invalid suggestion. Tried to apply a 'string' to a 'number' input",
+      );
+      return;
+    }
 
-		inputRef.current.value = suggestion.value;
-		onChange?.(suggestion.value);
-	};
+    onChange?.(suggestion.value);
+  };
 
-	const handleSuggestionClick: MouseEventHandler = () => {
-		applySuggestion(suggestion);
-	};
-	const handleSuggestionKeyDown: KeyboardEventHandler = (e) => {
-		e.preventDefault();
+  const handleSuggestionClick: MouseEventHandler = () => {
+    applySuggestion(suggestion);
+  };
+  const handleSuggestionKeyDown: KeyboardEventHandler = (e) => {
+    e.preventDefault();
 
-		if (e.code === "Space" || e.code === "Enter") {
-			applySuggestion(suggestion);
-		}
-	};
+    if (e.code === "Space" || e.code === "Enter") {
+      applySuggestion(suggestion);
+    }
+  };
 
-	const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-		const value = e.target.value;
-		if (!isValid(value)) return;
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const value = e.target.value;
+    if (!isValid(value)) return;
 
-		onChange?.(value);
-	};
+    onChange?.(value);
+  };
 
-	return (
-		<S.Container>
-			{label && <label htmlFor={props.id}>{label}</label>}
+  return (
+    <S.Container>
+      {label && <label htmlFor={props.id}>{label}</label>}
 
-			<S.InputContainer>
-				{prefix && (
-					<>
-						<S.Prefix as="span">{prefix}</S.Prefix>
-						<Text css={{ lineHeight: "100%", color: "$secondary" }}>|</Text>
-					</>
-				)}
+      <S.InputContainer>
+        {prefix && (
+          <>
+            <S.Prefix as="span">{prefix}</S.Prefix>
+            <Text css={{ lineHeight: "100%", color: "$secondary" }}>|</Text>
+          </>
+        )}
 
-				<S.Input
-					type="text"
-					value={value}
-					css={css}
-					ref={inputRef}
-					onChange={handleChange}
-					{...props}
-				/>
+        <S.Input
+          type="text"
+          value={value}
+          css={css}
+          ref={inputRef}
+          onChange={handleChange}
+          {...props}
+        />
 
-				{suggestion && isValueEmpty && (
-					<>
-						<Text css={{ lineHeight: "100%", color: "$secondary" }}>|</Text>
-						<Suggestion
-							onClick={handleSuggestionClick}
-							onKeyDown={handleSuggestionKeyDown}
-							tabIndex={0}
-						>
-							{suggestion.label}
-						</Suggestion>
-					</>
-				)}
+        {suggestion && isValueEmpty && (
+          <>
+            <Text css={{ lineHeight: "100%", color: "$secondary" }}>|</Text>
+            <Suggestion
+              onClick={handleSuggestionClick}
+              onKeyDown={handleSuggestionKeyDown}
+              tabIndex={0}
+            >
+              {suggestion.label}
+            </Suggestion>
+          </>
+        )}
 
-				{suffix}
-			</S.InputContainer>
+        {suffix}
+      </S.InputContainer>
 
-			{helper && (
-				<Label as="span" size="s">
-					{helper}
-				</Label>
-			)}
-		</S.Container>
-	);
+      {helper && (
+        <Label as="span" size="s">
+          {helper}
+        </Label>
+      )}
+    </S.Container>
+  );
 }
