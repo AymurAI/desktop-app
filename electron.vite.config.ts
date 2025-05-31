8f0468289f3c2d2be3e9339cc5aecd34447af150
AymurAI Frontend <front-end@datagenero.org>
2025-05-31 18:04:48 -0300

    build: replace electron scripts by electron-vite scripts and plugin

---
+++ electron.vite.config.ts
@@ -0,0 +1,20 @@
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  preload: {
+    plugins: [externalizeDepsPlugin()],
+  },
+  renderer: {
+    resolve: {
+      alias: {
+        "@": resolve("src/renderer/src"),
+      },
+    },
+    plugins: [react()],
+  },
+});
