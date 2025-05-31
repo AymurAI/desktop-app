6ed9b7be36407b259499929436fecb4367d9b834
AymurAI Frontend <front-end@datagenero.org>
2025-05-31 16:52:14 -0300

    chore(design): remove console.log from select option filtering

--- src/components/design/select/utils/filtering.ts
+++ src/components/design/select/utils/filtering.ts
@@ -3,12 +3,10 @@ import { includes } from "utils/regex";
 import type { SelectOption } from "..";
 
 export function filterOptions(options: SelectOption[], inputValue: string) {
-  if (!inputValue) return options;
+	if (!inputValue) return options;
 
-  return options.filter(({ label, value }) => {
-    console.log(label, value);
-    return (
-      label.match(includes(inputValue)) || value.match(includes(inputValue))
-    );
-  });
+	return options.filter(
+		({ label, value }) =>
+			label.match(includes(inputValue)) || value.match(includes(inputValue)),
+	);
 }
