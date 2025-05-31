49039b55b8eeb37bebaa276c9b12e9e603655078
AymurAI Frontend <front-end@datagenero.org>
2025-05-31 14:44:10 -0300

    feat(design): create controlled radio group component

---
+++ src/components/design/radio-group/Radio.tsx
@@ -0,0 +1,42 @@
+import { Circle } from "phosphor-react";
+
+import { colors } from "styles/tokens";
+import * as S from "./RadioGroup.styles";
+import type { Radio as SingelRadio } from "./index";
+
+interface RadioProps extends SingelRadio {
+	checked: boolean;
+	groupName: string;
+	onChange: (value: string) => void;
+}
+
+export function Radio({
+	id,
+	label,
+	checked,
+	disabled,
+	groupName,
+	onChange,
+	value,
+}: RadioProps) {
+	const iconColor = disabled
+		? colors.textOnButtonDisabled
+		: colors.textOnButtonAlternative;
+
+	return (
+		<S.RadioWrapper hasText={!!label} isDisabled={disabled}>
+			<S.Input
+				id={id}
+				type="radio"
+				name={groupName}
+				checked={checked}
+				disabled={disabled}
+				onChange={() => onChange(value)}
+			/>
+			<S.RadioCircle>
+				<Circle color={iconColor} weight="fill" />
+			</S.RadioCircle>
+			{label}
+		</S.RadioWrapper>
+	);
+}
