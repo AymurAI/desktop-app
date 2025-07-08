d02840705f5e29af0cb48070ed30c32389a6a3b3
AymurAI Frontend <front-end@datagenero.org>
2025-07-07 21:57:07 -0400

    build: add vite config for dev web server

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,6 @@
-import { resolve } from "node:path";
 import react from "@vitejs/plugin-react";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
+import { resolve } from "node:path";
 
 export default defineConfig({
   main: {
