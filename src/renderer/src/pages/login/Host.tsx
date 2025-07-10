a998bfafdd68d99599ea2398665fd00e25ad0b58
AymurAI Frontend <front-end@datagenero.org>
2025-07-10 17:28:45 -0400

    chore(login): add support for local server start

--- src/renderer/src/pages/login/Host.tsx
+++ src/renderer/src/pages/login/Host.tsx
@@ -26,73 +26,34 @@ export function Host() {
   const { setServerHost } = localStore.useServerHostActions();
   const { mutate: connectToHost, isPending, error, reset } = useConnectToHost();
 
-  // const [isConnected, setIsConnected] = useState(false);
-  // const [error, setError] = useState("");
-  // /**
-  //  * Ensures the user state has been successfully updated before navigating to the `/home`
-  //  */
-  // useEffect(() => {
-  //   if (user && user.function !== "") navigate("/onboarding");
-  // }, [user, navigate]);
-
-  // const handleUrlSubmit = async () => {
-  //   // const response = await getHealthCheck(inputValue);
-  //   // if (response === 200) {
-  //   //   setHasToChooseUrl(false);
-  //   //   localStorage.setItem("serverUrl", inputValue);
-  //   //   setError("");
-  //   //   setServerUrl(inputValue);
-  //   //   setIsConnected(true);
-  //   //   return;
-  //   // }
-  //   // if (response === "Request failed with status code 404") {
-  //   //   setError("La dirección ingresada no es correcta");
-  //   // } else {
-  //   //   setError("En este momento no es posible conectarse con el servidor");
-  //   // }
-  // };
-
-  // const handleLocalConnection = async () => {
-  //   if (window.electronAPI) {
-  //     try {
-  //       await window.electronAPI.runBatch();
-  //       console.log("Server is running in the background.");
-  //     } catch (error) {
-  //       console.error(
-  //         "Failed to run batch. Please run the server manually.",
-  //         error,
-  //       );
-  //       alert(
-  //         "No se pudo inicializar el servidor automáticamente. Por favor inícialo manualmente.",
-  //       );
-  //     }
-  //   } else {
-  //     console.warn("Electron API not available. Unable to run the batch file.");
-  //     alert(
-  //       "No se puede inicializar el servidor automáticamente. Por favor, inícialo manualmente.",
-  //     );
-  //   }
-
-  //   // Update state and storage regardless of the batch process result
-  //   setIsLocal(true);
-  //   setServerUrl("");
-  //   setError("");
-  //   localStorage.setItem("serverUrl", "");
-  // };
-
-  // useEffect(() => {
-  //   axios.defaults.baseURL = inputValue;
-
-  //   axios.get("/").then(console.log).catch(console.error);
-  // }, [inputValue]);
-
   const handleBack = () => {
     setIsLocal(null);
   };
 
-  const handleUseLocal = () => {
-    setIsLocal(true);
+  const handleUseLocal = async () => {
+    if (window.electronAPI) {
+      try {
+        await window.electronAPI.runBatch();
+        console.log("Server is running in the background.");
+
+        navigate("/login/features");
+      } catch (error) {
+        console.error(
+          "Failed to run batch. Please run the server manually.",
+          error,
+        );
+        alert(
+          "No se pudo inicializar el servidor automáticamente. Por favor inícialo manualmente.",
+        );
+      }
+    } else {
+      console.warn("Electron API not available. Unable to run the batch file.");
+      alert(
+        "No se puede inicializar el servidor automáticamente. Por favor, inícialo manualmente.",
+      );
+    }
   };
+
   const handleUseRemote = () => {
     setIsLocal(false);
   };
