e4b2f8426332da2ce7503dac24bd07869a790807
AymurAI Frontend <front-end@datagenero.org>
2025-08-10 12:56:11 -0300

    chore: remove auth context and related hooks

--- src/renderer/src/hooks/index.ts
+++ src/renderer/src/hooks/index.ts
@@ -1,9 +1,7 @@
 import { useFileDispatch, useFiles } from "./useFiles";
 import useForm from "./useForm";
-import useLogin from "./useLogin";
-import useUser from "./useUser";
 
 export { useFileParser } from "./useFileParser";
 export { usePredict } from "./usePredict";
 
-export { useFileDispatch, useFiles, useForm, useLogin, useUser };
+export { useFileDispatch, useFiles, useForm };
