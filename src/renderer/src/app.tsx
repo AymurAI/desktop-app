076f54d8982ee06f7004e8fe6615cfa966e93528
AymurAI Frontend <front-end@datagenero.org>
2025-12-01 11:33:44 -0300

    chore: remove comments

--- src/renderer/src/app.tsx
+++ src/renderer/src/app.tsx
@@ -1,7 +1,7 @@
 import {
+  RouterProvider,
   createMemoryHistory,
   createRouter,
-  RouterProvider,
 } from "@tanstack/react-router";
 
 import { ThemeProvider } from "@/components";
@@ -27,39 +27,6 @@ declare module "@tanstack/react-router" {
     router: typeof router;
   }
 }
-
-// const router = createRouter([
-//   {
-//     path: "/",
-//     element: <Navigate to="/login" />,
-//   },
-//   {
-//     path: "/login",
-//     element: <LoginLayout />,
-//     children: [
-//       { index: true, element: <LoginHost /> },
-//       { path: "host", element: <LoginHost /> },
-//       { path: "features", element: <LoginFeatures /> },
-//     ],
-//   },
-//   {
-//     // Main as a layout element
-//     path: "app/:feature",
-//     element: <MainLayout />,
-//     children: [
-//       // 1. Onboarding. Explanation of the workflow to process files
-//       { path: "onboarding", element: <Onboarding /> },
-//       // 2. Preview. Process the file to get its text contents
-//       { path: "preview", element: <Preview /> },
-//       // 3. Process. Passes the text contents to the AI to extract predictions
-//       { path: "process", element: <Process /> },
-//       // 4. Validation. Validate the predictions/anonymize the file
-//       { path: "validation", element: <Validation /> },
-//       // 5. Finish. Show the results and download output files
-//       { path: "finish", element: <Finish /> },
-//     ],
-//   },
-// ]);
 
 export default function App() {
   return (
