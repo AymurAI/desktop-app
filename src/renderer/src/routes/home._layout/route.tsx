2da311cebc9728431177ad6f8855f18e2f425331
AymurAI Frontend <front-end@datagenero.org>
2025-12-06 17:29:59 -0300

    chore: kill cache when entering home layout

--- src/renderer/src/routes/home._layout/route.tsx
+++ src/renderer/src/routes/home._layout/route.tsx
@@ -3,6 +3,7 @@ import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/home/_layout")({
   component: RouteComponent,
+  onEnter: ({ context }) => context.queryClient.removeQueries(),
 });
 
 function RouteComponent() {
