a2c2040ae0979800c5b7ccf80b43cf80710273ae
AymurAI Frontend <front-end@datagenero.org>
2026-02-21 12:53:09 -0300

    fix: replace brand images with correct ones and set proper heights

--- src/renderer/src/components/home/choose-host.tsx
+++ src/renderer/src/components/home/choose-host.tsx
@@ -20,11 +20,9 @@ export default function ChooseHost({ onRemoteClick }: ChooseHostProps) {
   return (
     <div className={stack({ align: "center", gap: "12", width: "[400px]" })}>
       <img
-        src="brand/aymurai-vert-darkpurple.png"
+        src="/brand/aymurai-vert-darkpurple.svg"
         alt="Logotipo AymurAI"
         width={180}
-        //  TODO: should get fixed with image changes
-        style={{ margin: -48 }}
       />
       <div className={stack({ align: "stretch", gap: "4", width: "full" })}>
         <h2
@@ -55,41 +53,4 @@ export default function ChooseHost({ onRemoteClick }: ChooseHostProps) {
       </div>
     </div>
   );
-  // return (
-  //   <Stack direction="column" align="center" style={{ gap: 48 }}>
-  //         <img
-  //           src="brand/aymurai-vert-darkpurple.png"
-  //           alt="Logotipo AymurAI"
-  //           width={180}
-  //           style={{ margin: -48 }}
-  //         />
-  //         <Stack direction="column" spacing="m" align="center">
-  //           <Subtitle size="s" weight="strong" as="h1">
-  //             ¿Como deseas conectarte a Aymurai?
-  //           </Subtitle>
-  //           <Stack
-  //             spacing="s"
-  //             direction="column"
-  //             align="center"
-  //             style={{ width: 400 }}
-  //           >
-  //             <Button
-  //               onClick={handleUseLocal}
-  //               disabled={isRunning}
-  //               isLoading={isRunning}
-  //             >
-  //               <Monitor weight="bold" />
-  //               Local
-  //             </Button>
-  //             <Subtitle size="s" aria-hidden>
-  //               o
-  //             </Subtitle>
-  //             <Button onClick={handleUseRemote}>
-  //               <HardDrives weight="bold" />
-  //               Servidor
-  //             </Button>
-  //           </Stack>
-  //         </Stack>
-  //       </Stack>
-  // )
 }
--- src/renderer/src/components/layout/header.tsx
+++ src/renderer/src/components/layout/header.tsx
@@ -1,46 +1,5 @@
-import { FEATURES } from "@/constants";
 import { css } from "@/styled/css";
 import { Divider, Grid, Stack, styled } from "@/styled/jsx";
-import type { FeatureFlowEnum } from "@/types/features";
-import { Link } from "@tanstack/react-router";
-import { DotsNine } from "phosphor-react";
-import FeatureIcon from "../feature-icon";
-import Button from "../ui/button";
-import Card from "../ui/card";
-import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-
-function Menu() {
-  const features = Object.entries(FEATURES);
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button size="icon-sm" style={{ padding: 2 }}>
-          <DotsNine size={32} />
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align="end">
-        <Grid columns={2} padding="4">
-          {features.map(([value, feature]) => (
-            <Link
-              key={value}
-              to="/app/$feature"
-              params={{ feature: value as FeatureFlowEnum }}
-            >
-              <Card size="sm">
-                <Stack gap="3" align="center">
-                  <FeatureIcon feature={value as FeatureFlowEnum} size="sm" />
-                  <styled.p textStyle="label.md.strong">
-                    {feature.title}
-                  </styled.p>
-                </Stack>
-              </Card>
-            </Link>
-          ))}
-        </Grid>
-      </PopoverContent>
-    </Popover>
-  );
-}
 
 const header = css({
   width: "full",
@@ -53,22 +12,17 @@ const header = css({
 interface HeaderProps {
   title?: string;
   children?: React.ReactNode;
-  withFeaturesMenu?: boolean;
 }
-export default function Header({
-  title,
-  withFeaturesMenu,
-  children,
-}: HeaderProps) {
+export default function Header({ title, children }: HeaderProps) {
   const img = title
-    ? "brand/aymurai-iso-darkpurple.svg"
-    : "brand/aymurai-hor-darkpurple.svg";
+    ? "/brand/aymurai-iso-darkpurple.svg"
+    : "/brand/aymurai-hor-darkpurple.svg";
 
   return (
     <div className={header}>
       <Grid columns={3} gap="4" alignItems="center">
         <Stack gap="4" align="center" direction="row">
-          <img width={200} src={img} alt="AymurAI logo" />
+          <img height={40} src={img} alt="AymurAI logo" />
           {title && (
             <>
               <Divider
@@ -81,10 +35,7 @@ export default function Header({
             </>
           )}
         </Stack>
-        <styled.div gridColumn="span 2">
-          {children}
-          {withFeaturesMenu && <Menu />}
-        </styled.div>
+        <styled.div gridColumn="span 2">{children}</styled.div>
       </Grid>
     </div>
   );
