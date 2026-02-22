beb63bc3b382848850ca04edf149617c1332aebb
AymurAI Frontend <front-end@datagenero.org>
2026-02-21 21:33:15 -0300

    chore: migrate hidden input

---
+++ src/renderer/src/components/hidden-input.tsx
@@ -0,0 +1,25 @@
+import { WHITELISTED_EXTENSIONS } from "@/constants";
+import { css, cx } from "@/styled/css";
+import type { ComponentProps } from "react";
+
+const input = css({
+  display: "none",
+  visibility: "hidden",
+  opacity: 0,
+  pointerEvents: "none",
+});
+
+interface HiddenInputProps extends ComponentProps<"input"> {}
+export default function HiddenInput({ className, ...props }: HiddenInputProps) {
+  // Convert the array into a '.dcox' form
+  const extensions = WHITELISTED_EXTENSIONS.map((ext) => `.${ext}`).join(",");
+  return (
+    <input
+      type="file"
+      accept={extensions}
+      tabIndex={-1}
+      className={cx(input, className)}
+      {...props}
+    />
+  );
+}
