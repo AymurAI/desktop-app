2eba84b3c5edbd5bc48b3f911eef5a880af98270
AymurAI Frontend <front-end@datagenero.org>
2026-02-21 21:19:40 -0300

    chore: refactor header so we can correctly position all elements

--- src/renderer/src/components/layout/header.tsx
+++ src/renderer/src/components/layout/header.tsx
@@ -1,42 +1,53 @@
 import { css } from "@/styled/css";
-import { Divider, Grid, Stack, styled } from "@/styled/jsx";
+import { Divider, Stack, styled } from "@/styled/jsx";
 
 const header = css({
+  position: "relative",
   width: "full",
+  height: "24",
   py: "6",
-  px: "8",
+  px: "12",
   bg: "bg.secondary",
   borderBottom: "[1px solid #BCBAB8]",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+});
+
+const centerSlot = css({
+  position: "absolute",
+  left: "[50%]",
+  transform: "translateX(-50%)",
 });
 
 interface HeaderProps {
   title?: string;
-  children?: React.ReactNode;
+  center?: React.ReactNode;
+  right?: React.ReactNode;
 }
-export default function Header({ title, children }: HeaderProps) {
+export default function Header({ title, center, right }: HeaderProps) {
   const img = title
     ? "/brand/aymurai-iso-darkpurple.svg"
     : "/brand/aymurai-hor-darkpurple.svg";
 
   return (
-    <div className={header}>
-      <Grid columns={3} gap="4" alignItems="center">
-        <Stack gap="4" align="center" direction="row">
-          <img height={40} src={img} alt="AymurAI logo" />
-          {title && (
-            <>
-              <Divider
-                orientation="vertical"
-                thickness="[2px]"
-                color="text.default"
-                height="4"
-              />
-              <styled.span textStyle="subtitle.md.strong">{title}</styled.span>
-            </>
-          )}
-        </Stack>
-        <styled.div gridColumn="span 2">{children}</styled.div>
-      </Grid>
-    </div>
+    <header className={header}>
+      <Stack gap="4" align="center" direction="row">
+        <img height={40} src={img} alt="AymurAI logo" />
+        {title && (
+          <>
+            <Divider
+              orientation="vertical"
+              thickness="[2px]"
+              color="text.default"
+              height="4"
+            />
+            <styled.span textStyle="subtitle.md.strong">{title}</styled.span>
+          </>
+        )}
+      </Stack>
+      {center && <div className={centerSlot}>{center}</div>}
+      <div>{right}</div>
+    </header>
   );
 }
