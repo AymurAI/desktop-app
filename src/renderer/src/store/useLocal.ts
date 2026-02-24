051b6bd43f291d3d6293d332c4159c99d14aae3c
AymurAI Frontend <front-end@datagenero.org>
2026-02-24 14:53:19 -0300

    chore: add tutorial seen to local storage store

--- src/renderer/src/store/useLocal.ts
+++ src/renderer/src/store/useLocal.ts
@@ -1,19 +1,35 @@
 import { create } from "zustand";
 import { devtools, persist } from "zustand/middleware";
+import { FeatureFlowEnum } from "../types/features";
+
+type TutorialsSeen = Record<FeatureFlowEnum, boolean>;
 
 interface LocalStorageStore {
   serverHost: string | null;
   setServerHost: (serverUrl: string) => void;
   clearServerHost: () => void;
+  tutorialsSeen: TutorialsSeen;
+  hasSeenTutorial: (feature: FeatureFlowEnum) => boolean;
+  setTutorialSeen: (feature: FeatureFlowEnum) => void;
 }
 
 const useLocalStore = create<LocalStorageStore>()(
   devtools(
     persist(
-      (set) => ({
+      (set, get) => ({
         serverHost: null,
         setServerHost: (serverHost: string) => set({ serverHost }),
         clearServerHost: () => set({ serverHost: null }),
+        tutorialsSeen: {
+          [FeatureFlowEnum.Anonymizer]: false,
+          [FeatureFlowEnum.Dataset]: false,
+        },
+        hasSeenTutorial: (feature: FeatureFlowEnum) =>
+          get().tutorialsSeen[feature],
+        setTutorialSeen: (feature: FeatureFlowEnum) =>
+          set((state) => ({
+            tutorialsSeen: { ...state.tutorialsSeen, [feature]: true },
+          })),
       }),
       {
         name: "local-storage",
@@ -22,6 +38,8 @@ const useLocalStore = create<LocalStorageStore>()(
   ),
 );
 
+// TODO: in the future, we should export all of these hooks under a single named exports
+// usage will be like the following: `localStore.useServerHost`
 export const useServerHost = () => useLocalStore((state) => state.serverHost);
 export const useServerHostActions = () => {
   const setServerHost = useLocalStore((state) => state.setServerHost);
@@ -32,3 +50,8 @@ export const useServerHostActions = () => {
     clearServerHost,
   };
 };
+
+export const useTutorialSeen = (feature: FeatureFlowEnum) =>
+  useLocalStore((state) => state.tutorialsSeen[feature]);
+export const useSetTutorialSeen = () =>
+  useLocalStore((state) => state.setTutorialSeen);
