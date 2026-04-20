707e844b5acf12d51e6d4b7ee4707b718574cb09
AymurAI Frontend <front-end@datagenero.org>
2026-04-19 22:27:45 -0300

    update suggestion mark component's styles

---
+++ src/renderer/src/components/ui/suggestion.tsx
@@ -0,0 +1,11 @@
+import { styled } from "@/styled/jsx";
+
+const Suggestion = styled("mark", {
+  base: {
+    bg: "bg.primary-alternative",
+    textStyle: "label.md.default",
+    cursor: "pointer",
+  },
+});
+
+export default Suggestion;
