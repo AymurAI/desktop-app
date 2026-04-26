ca56bb635a1ec31206435a76275de5dbaeb5593a
AymurAI Frontend <front-end@datagenero.org>
2026-04-26 20:40:22 -0300

    add size variant to suggestion and adjust display

--- src/renderer/src/components/ui/suggestion.tsx
+++ src/renderer/src/components/ui/suggestion.tsx
@@ -4,15 +4,21 @@ const Suggestion = styled("mark", {
   base: {
     bg: "bg.primary-alternative",
     textStyle: "label.md.default",
+    display: "inline-flex",
   },
   variants: {
     clickable: {
       true: { cursor: "pointer" },
       false: { cursor: "unset" },
     },
+    rounded: {
+      true: { rounded: "md" },
+      false: { rounded: "[0px]" },
+    },
   },
   defaultVariants: {
     clickable: false,
+    rounded: false,
   },
 });
 
