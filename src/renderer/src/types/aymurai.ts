6f8a478c245236e4c7ae11b83bad30712bf07600
AymurAI Frontend <front-end@datagenero.org>
2026-04-27 04:32:01 -0300

    adjust typings

--- src/renderer/src/types/aymurai.ts
+++ src/renderer/src/types/aymurai.ts
@@ -1,4 +1,4 @@
-import type { SelectOption } from "@/components/select";
+import type { SelectOption } from "@/components/ui/select";
 
 // ------------
 // PREDICTION
@@ -12,6 +12,7 @@ type LabelAttributes = {
   aymurai_alt_text: string | null;
   aymurai_alt_start_char: number | null;
   aymurai_alt_end_char: number | null;
+  canonical_entity_id?: string | null;
 };
 
 export type PredictLabel = {
@@ -88,10 +89,10 @@ export enum LabelAnonimizer {
   USUARIX = "USUARIX",
 }
 
-export type AllLabels = LabelType | LabelDecisiones | LabelAnonimizer;
+export type AllLabels = LabelType | LabelDecisiones | LabelAnonimizer | AnonymizerLabels;
 export type AllLabelsWithSufix = `${AllLabels}_${number}`;
 
-export const anonymizerLabels: SelectOption[] = [
+export const anonymizerLabels = [
   { id: "DNI", text: "DNI" },
   { id: "PER", text: "Persona" },
   { id: "TEL", text: "Número de teléfono" },
@@ -122,7 +123,9 @@ export const anonymizerLabels: SelectOption[] = [
   { id: "NUM_MATRICULA", text: "Número matrícula" },
   { id: "PATENTE_DOMINIO", text: "Patente dominio" },
   { id: "TEXTO_ANONIMIZAR", text: "Texto anonimizar" },
-];
+] as const satisfies SelectOption[];
+
+export type AnonymizerLabels = (typeof anonymizerLabels)[number]["id"];
 
 // --------------------
 // DOCUMENT EXTRACTION
