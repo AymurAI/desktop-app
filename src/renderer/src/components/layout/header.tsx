5663da42465007602ef138ea74b789271514f916
AymurAI Frontend <front-end@datagenero.org>
2026-05-11 21:57:51 -0300

    make aymurai's logo a link in the header

--- src/renderer/src/components/layout/header.tsx
+++ src/renderer/src/components/layout/header.tsx
@@ -2,6 +2,7 @@ import { useTutorialSeen } from "@/store/useLocal";
 import { css } from "@/styled/css";
 import { Divider, HStack, Stack, styled } from "@/styled/jsx";
 import type { FeatureFlowEnum } from "@/types/features";
+import { Link } from "@tanstack/react-router";
 import FeaturesMenu from "../features-menu";
 import HowItWorksModal from "../how-it-works-modal";
 
@@ -51,20 +52,22 @@ export default function Header({ title, center, feature, right }: HeaderProps) {
 
   return (
     <header className={header}>
-      <Stack gap="4" align="center" direction="row">
-        <img height={40} src={img} alt="AymurAI logo" />
-        {title && (
-          <>
-            <Divider
-              orientation="vertical"
-              thickness="[2px]"
-              color="text.default"
-              height="4"
-            />
-            <styled.span textStyle="subtitle.md.strong">{title}</styled.span>
-          </>
-        )}
-      </Stack>
+      <Link to="/home/features">
+        <Stack gap="4" align="center" direction="row">
+          <img height={40} src={img} alt="AymurAI logo" />
+          {title && (
+            <>
+              <Divider
+                orientation="vertical"
+                thickness="[2px]"
+                color="text.default"
+                height="4"
+              />
+              <styled.span textStyle="subtitle.md.strong">{title}</styled.span>
+            </>
+          )}
+        </Stack>
+      </Link>
       {center && <div className={centerSlot}>{center}</div>}
       {rightSlot}
     </header>
